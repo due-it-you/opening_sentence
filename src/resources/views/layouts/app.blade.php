50ebdbdf88d77f8612bb9eff77d8a18a865b12bb
dueityou <dueityou@gmail.com>
2025-12-17 17:30:02 +0900

    feat: #2 全てに共通する親テンプレートを作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>書き出し小説 - @yield('title')</title>
+  </head>
+  <body>
+    <div>
+      <div class="container">
+        @yield('content')
+      </div>
+    </div>
+  </body>
+</html>
