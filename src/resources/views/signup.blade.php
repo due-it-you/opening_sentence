b84f4e7614817889a71cd12a242eb50a47fb2d27
dueityou <dueityou@gmail.com>
2025-12-18 15:07:25 +0900

    feat: #3 新規登録フォームの作成

--- src/resources/views/signup.blade.php
+++ src/resources/views/signup.blade.php
@@ -10,6 +10,25 @@
 
 @section('content')
   <div>
-    新規登録
+    <div class="font-bold">
+      アカウント新規登録
+    </div>
+    <!-- 新規登録フォーム -->
+    <div>
+      <form action="POST" action="">
+        @csrf
+        <div>
+          <label for="email">メールアドレス</label>
+          <input id="email" name="email" type="email" class="border border-gray-600" placeholder="xxxx@example.com">
+        </div>
+        <div class="mt-4">
+          <label for="password">パスワード</label>
+          <input id="password" name="password" type="password" class="border border-gray-600" placeholder="パスワード">
+        </div>
+        <div class="mt-4">
+          <input type="submit" value="送信" class="border px-2 rounded-md">
+        </div>
+      </form>
+    </div>
   </div>
 @endsection
