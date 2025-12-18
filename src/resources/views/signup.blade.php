06c681476e8ca6eb054f8ace455b8935a9aed438
dueityou <dueityou@gmail.com>
2025-12-18 14:26:12 +0900

    feat: #3 新規登録ページのファイルの新規作成

---
+++ src/resources/views/signup.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', '新規登録')
+
+@section('header')
+  <div class="bg-gray-200 h-16">
+    <a href="">新規登録</a>
+  </div>
+@endsection
+
+@section('content')
+  <div>
+    
+  </div>
+@endsection
