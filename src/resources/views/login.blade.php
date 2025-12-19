b2c932170e00978be33c20d4b87acf6357e54e3e
ideyuuto <dueityou@gmail.com>
2025-12-19 18:17:31 +0900

    feat: #5 ログインフォームの仮実装

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -2,14 +2,19 @@
 
 @section('title', '新規登録')
 
-@section('header')
-  <div class="bg-gray-200 h-16">
-    <a href="">新規登録</a>
-  </div>
-@endsection
-
 @section('content')
     <div>
         ログイン
+        <form action="" method="POST">
+            @csrf
+            <div>
+                <label for="email">メールアドレス</label>
+                <input type="email" name="email" id="email" class="border">
+            </div>
+            <div>
+                <label for="password">パスワード</label>
+                <input type="password" name="password" id="password" class="border">
+            </div>
+        </form>
     </div>
 @endsection
