b7a5bb9de3b15712157029afa7a92f3820afa87e
ideyuuto <dueityou@gmail.com>
2025-12-20 00:09:18 +0900

    feat: #8 ログアウト処理の実装

--- src/app/Http/Controllers/AuthSessionController.php
+++ src/app/Http/Controllers/AuthSessionController.php
@@ -24,4 +24,15 @@ class AuthSessionController extends Controller
             'login' => 'メールアドレスまたはパスワードが正しくありません。'
         ]);
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
