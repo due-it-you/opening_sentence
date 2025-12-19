b8be42f0348f053fa137914e94486e4f00fcf392
ideyuuto <dueityou@gmail.com>
2025-12-19 22:57:14 +0900

    feat: #6 認証処理の実装

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginAuthenticateRequest;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function authenticate(LoginAuthenticateRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            # セッション固定攻撃を防ぐためのセッションの再生成
+            $request->session()->regenerate();
+
+            return redirect()->intended('top');
+        }
+
+        return back()->withErrors([
+            'login' => 'メールアドレスまたはパスワードが正しくありません。'
+        ]);
+    }
 }
