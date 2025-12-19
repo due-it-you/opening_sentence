00c46bfd0cc900ad60192929a1f662004ed34e93
ideyuuto <dueityou@gmail.com>
2025-12-19 22:24:04 +0900

    fix: #6 重複している行を削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SignupController;
-use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('top');
