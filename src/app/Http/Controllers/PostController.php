6101986c19411a79c8951f1cf35e1d921bddab32
ideyuuto <dueityou@gmail.com>
2025-12-20 23:23:10 +0900

    feat: #11 ログイン中のユーザーの全ての投稿をビューに渡す

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Auth::user()->posts()->get();
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
