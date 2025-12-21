829423eadf6e6430e8af73eb4af363ace00f5e3b
ideyuuto <dueityou@gmail.com>
2025-12-21 14:35:47 +0900

    feat: #12 投稿更新機能における認可の処理の実装

---
+++ src/app/Policies/PostPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Post;
+
+class PostPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * 指定された投稿をユーザーが更新可能か判定
+     */
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->id;
+    }
+}
