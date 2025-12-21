bce3cf49c2eaca3355cc2083ec3fbf1aeb6705f2
ideyuuto <dueityou@gmail.com>
2025-12-21 12:38:08 +0900

    feat: #12 投稿の編集ボタンのUIの追加

--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -5,8 +5,13 @@
 @section('content')
     <div>
         @foreach ($posts as $post)
-            <div class="border">
-                {{ $post->body }}
+            <div class="flex space-y-4 mt-4">
+                <div class="border">
+                    {{ $post->body }}
+                </div>
+                <div>
+                    <a href="" class="border px-2 py-1">編集</a>
+                </div>
             </div>
         @endforeach
     </div>
