570cd4cf1d6567e986027aedb97701018d374f1e
dueityou <dueityou@gmail.com>
2025-12-25 07:46:51 +0900

    feat: #15 管理者ログイン処理の実装

--- src/app/Http/Controllers/AdminLoginController.php
+++ src/app/Http/Controllers/AdminLoginController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginAuthenticateRequest;
 
 class AdminLoginController extends Controller
 {
-    //
+    public function authenticate(LoginAuthenticateRequest $request)
+    {
+        $credentials = $request->validated();
+        if (Auth::guard('admin')->attempt($credentials))
+        {
+            $request->session()->regenerate();
+            return redirect()->intended('/admin/dashboard')->with('success', '管理者としてログインしました。');
+        }
+
+        return back()->withErrors([
+            'login' => 'ログインに失敗しました。'
+        ]);
+    }
 }
