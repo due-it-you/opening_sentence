9f3a32e562b73df1494a982ce9520d1812b5405c
dueityou <dueityou@gmail.com>
2025-12-25 10:47:45 +0900

    fix: #15 モデルの読み込みのパスの修正

--- src/database/seeders/AdminUserSeeder.php
+++ src/database/seeders/AdminUserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Http\Models\AdminUser;
+use App\Models\AdminUser;
 
 class AdminUserSeeder extends Seeder
 {
