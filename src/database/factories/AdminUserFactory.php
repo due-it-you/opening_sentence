0dd985bff6c78e1a3d9286dabf3c45ec81a57383
dueityou <dueityou@gmail.com>
2025-12-25 10:44:36 +0900

    feat: #15 管理者ユーザーのダミーデータの内容の定義

---
+++ src/database/factories/AdminUserFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AdminUser>
+ */
+class AdminUserFactory extends Factory
+{
+    /**
+     * ファクトリが現在使用しているパスワード
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => static::$password ??= Hash::make('password')
+        ];
+    }
+}
