d64d07f76266750c28eca69daef70059c0555aa0
dueityou <dueityou@gmail.com>
2025-12-25 11:27:53 +0900

    feat: #15 管理者用ログアウトボタンの実装

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -5,5 +5,11 @@
 @section('content')
   <div>
     管理者ダッシュボード
+    <div>
+      <form action="{{ route('admin.logout') }}" method="POST">
+        @csrf
+        <button type="submit" class="bg-red-500 text-white font-bold">ログアウト</button>
+      </form>
+    </div>
   </div>
 @endsection
