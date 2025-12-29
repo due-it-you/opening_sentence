9bc0ddb092d4429d1eca524e1abd3453183e62cc
dueityou <dueityou@gmail.com>
2025-12-29 16:24:35 +0900

    test: #39 不正な値の入力で一般ユーザーの認証処理が失敗した場合の機能テストを実装

--- src/tests/Feature/Http/Controllers/UserAuthControllerTest.php
+++ src/tests/Feature/Http/Controllers/UserAuthControllerTest.php
@@ -35,4 +35,32 @@ class UserAuthControllerTest extends TestCase
         $response->assertRedirect('/');
         $response->assertSessionHas('success', 'ログインしました。');
     }
+
+    /**
+     * 異常系：
+     * 不正な値によって一般ユーザーの認証処理が失敗した場合のテスト
+     */
+    public function test_user_authenticate_with_invalid_input(): void
+    {
+        # 準備
+        $plain_password = 'test_password';
+        $user = User::factory()->create([
+            'password' => Hash::make($plain_password)
+        ]);
+        $invalid_password = 'invalid_password';
+        
+        # HTTPリクエスト
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            # 不正なパスワードを入力
+            'password' => $invalid_password
+        ]);
+
+        # アサーション
+        $this->assertGuest();
+        $response->assertRedirectBack();
+        $response->assertSessionHasErrors([
+            'login' => 'メールアドレスまたはパスワードが正しくありません。'
+        ]);
+    }
 }
