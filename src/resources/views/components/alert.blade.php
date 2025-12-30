fd528e08a6f0595490a92150c0fcfa67ea2bc27c
dueityou <dueityou@gmail.com>
2025-12-30 18:03:15 +0900

    refactor: #48 エラーメッセージ表示箇所をcomponentsファイルに切り出し

---
+++ src/resources/views/components/alert.blade.php
@@ -0,0 +1,10 @@
+<!-- エラーメッセージの表示 -->
+<div>
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+</div>
